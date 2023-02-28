7e30375395b6f8348a0b3af827f0e37820643e24
zidaann <jourjizaidan8@gmail.com>
2023-02-28 21:04:58 +0700

    update ui

---
+++ src/pages/doa/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../../layouts/App';
+
+export default function Index() {
+   return (
+      <App title={'Doa'}>
+         <div className='container' id='menu'>
+            <div className='row'>Ini Doa</div>
+         </div>
+      </App>
+   );
+}
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import * as Quran from '../pages/quran/App';
+import * as Doa from '../pages/doa/App';
 export default function ReactRouter() {
    return (
       <Router>
          <Routes>
             <Route path='/' element={<Home />}></Route>
-            <Route path='/quran/surah' element={<Quran.Index />}></Route>
+            <Route exact path='/quran/surah' element={<Quran.Index />}></Route>
             <Route path='/quran/surah/:nomor' element={<Quran.Show />}></Route>
+            <Route exact path='/doa' element={<Doa.Index />}></Route>
          </Routes>
       </Router>
    );
